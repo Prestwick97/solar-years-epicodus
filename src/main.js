b226f9381838c641b24fa5cae1a47b4a3fcfad76
jeremy kale padot and patrick delaney <jeremy@>
2020-02-05 10:45:26 -0800

    add webpack template

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// User Interface Logic
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import {  } from './project-name';
+
+$
