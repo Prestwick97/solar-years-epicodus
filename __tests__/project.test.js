716126798e1c67d4f95020329c56e327fb515bb5
Prestwick97 <prestwick97@gmail.com>
2020-02-07 13:34:35 -0800

    refactor testing - all still pass

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -3,23 +3,35 @@ import { PlanetaryYears } from "./../src/solar-logic"
 describe('mercuryAge', () => {
   test('should correctly provide age on mercury with inputted earth age', () => {
     let planetaryYears = new PlanetaryYears();
-    expect(planetaryYears.mercuryAge()).toEqual();
+    planetaryYears.mercuryAge();
+    expect(planetaryYears.planetDif).toEqual(0.24);
   })
 })
 
 describe('venusAge', () => {
   test('should correctly provide age on venus with inputted earth age', () => {
     let planetaryYears = new PlanetaryYears();
-    expect(planetaryYears.venusAge()).toEqual();
+    planetaryYears.venusAge();
+    expect(planetaryYears.planetDif).toEqual(0.62);
   })
 })
 
 describe('marsAge', () => {
   test('should correctly provide age on mars with inputted earth age', () => {
     let planetaryYears = new PlanetaryYears();
-    expect(planetaryYears.marsAge()).toEqual();
+    planetaryYears.marsAge();
+    expect(planetaryYears.planetDif).toEqual(1.88);
   })
 })
+
+describe('jupiterAge', () => {
+  test('should correctly provide age on jupiter with inputted earth age', () => {
+    let planetaryYears = new PlanetaryYears();
+    planetaryYears.jupiterAge();
+    expect(planetaryYears.planetDif).toEqual(11.86);
+  })
+})
+
 
 describe('jupiterAge', () => {
   test('should correctly provide age on jupiter with inputted earth age', () => {
--- src/solar-logic.js
+++ src/solar-logic.js
@@ -1,13 +1,17 @@
 // Business Logic
 export class PlanetaryYears {
-  constructor(earthAge, planetAge, planetDif, lifeExpectancy) {
+  constructor(earthAge, planetAge, planetDif, lifeExpectancy, planetExpectancy) {
     this.earthAge = earthAge;
     this.planetAge = planetAge;
     this.planetDif = planetDif;
+    this.planetExpectancy = planetExpectancy;
     this.lifeExpectancy = lifeExpectancy;
   }
   planetaryAge() {
     this.planetAge = this.earthAge / this.planetDif;
+  }
+  planetaryEcpectancy() {
+    this.planetExpectancy = this.lifeExpectancy / this.planetDif; 
   }
   mercuryAge() {
     this.planetDif = 0.24;
@@ -20,8 +24,5 @@ export class PlanetaryYears {
   }
   jupiterAge() {
     this.planetDif = 11.86;
-  }
-  planetaryEcpectancy() {
-    this.lifeExpectancy
   }
 };
