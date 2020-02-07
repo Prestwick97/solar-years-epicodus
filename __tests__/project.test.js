c8bd041aebc31962c340863810e804657da38496
Prestwick97 <prestwick97@gmail.com>
2020-02-07 10:58:59 -0800

    fix describe statement from auto-corrected statement

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -1,9 +1,9 @@
 import { PlanetaryYears } from "./../src/solar-logic"
 
 describe('mercuryAge', () => {
-  TextDecoderStream('should correctly provide age on mercury with inputted earth age', () => {
+  describe('should correctly provide age on mercury with inputted earth age', () => {
     let planetaryYears = new PlanetaryYears();
     let earthAge = 22;
-    expect(planetaryYears.mercuryYears()).toEqual()
+    expect(planetaryYears.mercuryYears()).toEqual(91.6)
   })
 })
