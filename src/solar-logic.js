54ffc49ce3c85c283cd0acf175e148259f60cdab
Prestwick97 <prestwick97@gmail.com>
2020-02-07 10:05:37 -0800

    create a class for planetary years

--- src/solar-logic.js
+++ src/solar-logic.js
@@ -1,2 +1,13 @@
 // Business Logic
-
+class PlanetaryYears {
+  constructor(earth, mercury, venus, mars, jupiter) {
+    this.earth = earth;
+    this.mercury = mercury;
+    this.venus = venus;
+    this.mars = mars;
+    this.jupiter = jupiter;
+  }
+  mercuryYears() {
+    earthAge
+  }
+}
