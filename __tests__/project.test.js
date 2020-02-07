a8ec352c9f2d9ef9d77aa4d30ce9f16a3a2d3ff3
Prestwick97 <prestwick97@gmail.com>
2020-02-07 11:18:49 -0800

    alter class constructor

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -3,7 +3,6 @@ import { PlanetaryYears } from "./../src/solar-logic"
 describe('mercuryAge', () => {
   describe('should correctly provide age on mercury with inputted earth age', () => {
     let planetaryYears = new PlanetaryYears();
-    let earthAge = 22;
     expect(planetaryYears.mercuryYears()).toEqual(91.6)
   })
 })
