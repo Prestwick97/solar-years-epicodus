bdc917cd950edcd0f70047ab94a9a75c33930c9f
Prestwick97 <prestwick97@gmail.com>
2020-02-07 13:09:55 -0800

    'blue' refactoring class

--- src/solar-logic.js
+++ src/solar-logic.js
@@ -1,21 +1,27 @@
 // Business Logic
 export class PlanetaryYears {
-  constructor(earthAge, planetAge, lifeExpectancy) {
+  constructor(earthAge, planetAge, planetDif, lifeExpectancy) {
     this.earthAge = earthAge;
     this.planetAge = planetAge;
+    this.planetDif = planetDif;
     this.lifeExpectancy = lifeExpectancy;
   }
+  planetaryAge() {
+    this.planetAge = this.earthAge / this.planetDif;
+  }
   mercuryAge() {
-    this.planetAge = this.earthAge/0.24;
+    this.planetDif = 0.24;
   }
   venusAge() {
-    this.planetAge = this.earthAge/0.62;
+    this.planetDif = 0.62;
   }
   marsAge() {
-    this.planetAge = this.earthAge/1.88;
+    this.planetDif = 1.88;
   }
   jupiterAge() {
-    this.planetAge = this.earthAge/11.86;
+    this.planetDif = 11.86;
   }
-
+  planetaryEcpectancy() {
+    this.lifeExpectancy
+  }
 };
