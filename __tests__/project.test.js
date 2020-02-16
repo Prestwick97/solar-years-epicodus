9959c0e0e1e713f0a05008ea4b5cbc1d04371898
Prestwick97 <prestwick97@gmail.com>
2020-02-15 16:24:09 -0800

    red, failed test for planetary expectancy

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -1,15 +1,15 @@
 import { PlanetaryYears } from "./../src/solar-logic";
 
 describe('planetaryYears', () => {
-  test('should correctly provide planetary "year" differentiation', () => {
-    let planetaryYears = new PlanetaryYears(22, 80);
-    planetaryYears.mercuryAge()
+  let planetaryYears = new PlanetaryYears(22, 80);
+  test('Should provide planetary age based on the differential of another planet.', () => {
+    planetaryYears.mercuryAge();
     expect(planetaryYears.planetAge).toEqual(91.66666666666667);
   })
-  // test('', () => {
-  
-  //   expect().toEqual();
-  // })
+  test('Should provide life expectancy based on the differential of another planet.', () => {
+    planetaryYears.mercuryExpectancy();
+    expect(planetaryYears.planetExpectancy).toEqual();
+  })
   // test('', () => {
     
   //   expect().toEqual();
