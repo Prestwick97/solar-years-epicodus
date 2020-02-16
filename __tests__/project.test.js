9647cf049c4a57bb689c8df2eaf845d0ef133fb1
Prestwick97 <prestwick97@gmail.com>
2020-02-15 18:08:06 -0800

    correct indentation

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -5,25 +5,25 @@ describe('planetaryYears', () => {
   test('Should provide planetary age based on the differential of another planet.', () => {
     planetaryYears.planetAge(0);
     expect(planetaryYears.planetAge).toEqual(91.66666666666667);
-  })
+  });
   test('Should provide life expectancy based on the differential of another planet.', () => {
     planetaryYears.planetExpectancy(0);
     expect(planetaryYears.planetExpectancy).toEqual(333.33333333333337);
-  })
+  });
   test('Should correctly calculate the time someone has left to live on another planet, assuming their age is less than their life expectancy.', () => {
     let planetaryYears = new PlanetaryYears(22, 80);
     planetaryYears.planetAge(0);
     planetaryYears.planetExpectancy(0);
     planetaryYears.expectancyRemainder();
     expect(planetaryYears.planetRemainder).toEqual(241.66666666666669);
-  })
+  });
   test('Should correctly calculate the time someone has left to live on another planet, assuming their age is less than their life expectancy.', () => {
     let planetaryYears = new PlanetaryYears(95, 80);
     planetaryYears.planetAge(0);
     planetaryYears.planetExpectancy(0);
     planetaryYears.expectancyRemainder();
     expect(planetaryYears.planetExcess).toEqual(62.5);
-  })
+  });
   
   test('calculatePlanet method should provide information for all properties using previous methods', () => {
     let planetaryYears = new PlanetaryYears(22, 80);
@@ -31,5 +31,5 @@ describe('planetaryYears', () => {
     expect(planetaryYears.planetAge).toEqual(91.66666666666667);
     expect(planetaryYears.planetExpectancy).toEqual(333.33333333333337);
     expect(planetaryYears.planetRemainder).toEqual(241.66666666666669);
-  })
+  });
 });
--- src/solar-logic.js
+++ src/solar-logic.js
@@ -4,26 +4,27 @@ export class PlanetaryYears {
     this.earthAge = earthAge;
     this.lifeExpectancy = lifeExpectancy;
     this.planetDif = [0.24, 0.62, 1.88, 11.86];
-  }
+  };
   planetAge(difNum) {
     this.planetAge = this.earthAge / this.planetDif[difNum];
-  }
+  };
   planetExpectancy(difNum) {
     this.planetExpectancy = this.lifeExpectancy / this.planetDif[difNum]; 
-  }
+  };
   expectancyRemainder() {
     if(this.planetAge > this.planetExpectancy){
       this.planetExcess = this.planetAge - this.planetExpectancy;
-      // return "Congratulations! You've servived" + this.planetExcess + "years past your life expectancy on this planet!";
+      return "Congratulations! You've survived " + this.planetExcess + " years past your life expectancy on this planet!";
     } else {
       this.planetRemainder = this.planetExpectancy - this.planetAge;
+      return "You have " + this.planetRemainder + " years left to live on this planet!";
     }
-  }
+  };
   calculatePlanet(difNum) {
     this.planetAge(difNum);
     this.planetExpectancy(difNum);
     this.expectancyRemainder();
-  }
+  };
 };
 
 
